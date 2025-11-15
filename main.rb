4c7e6520f649a8727eece7a8298a7b115b8172c1
didi <mnkimberly1@gmail.com>
2025-11-15 18:19:19 -0300

    mini project super mixed, with ruby c:

--- main.rb
+++ main.rb
@@ -1,4 +1,55 @@
-#!/usr/bin/env ruby
+#+BEGIN_SRC ruby
+
+first_name = "Kim"
+last_name = "Melo"
+full_name = first_name + " " + last_name
+
+# puts "Hello world"
+# puts first_name
+
+puts "Salut, " + full_name
+
+books = {}
+books["1984"] = :splendid
+
+x = -7
+y = 9
+# z = -15
+# z = x + y
+
+# 23 + 50 = 73
+
+# puts "x + y = " + x.to_s + y.to_s
+# puts "x + y = " + x.to_s + y.to_s
+# puts "x + y = " + z.to_s
+puts "x + y = " + (x + y).to_s
+
+puts 4 * 17
 
 
-puts "salut!"
+
+
+# true or false
+# Booleans are either true or false.
+
+person_is_able_to_drive = true
+person_is_unable_to_drive = false
+
+
+persons_age = 9
+
+z = x = y
+
+result = true
+
+if persons_age >= 18
+  puts "you can drive! c:"
+else
+  puts "you can't drive! Ç:"
+end
+
+
+
+
+
+#+END_SRC
